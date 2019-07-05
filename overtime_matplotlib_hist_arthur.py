2b95ee02316794f14c51bc0280ab3ff5eac9b104
Dennis Jackson <djackson@mozilla.com>
2019-07-05 13:47:59 -0700

    Tidy up overtime code

--- overtime_matplotlib_hist_arthur.py
+++ overtime_matplotlib_hist_arthur.py
@@ -13,36 +13,14 @@ from scipy.stats import gaussian_kde
 from sklearn.neighbors import KernelDensity
 from scipy.interpolate import interpn
 import matplotlib.dates as mdates
-
-
-#https://stackoverflow.com/questions/20105364/how-can-i-make-a-scatter-plot-colored-by-density-in-matplotlib
-def density_scatter( x , y, ax = None, sort = True, bins = 20, **kwargs )   :
-    """
-    Scatter plot colored by 2d histogram
-    """
-    if ax is None :
-        fig , ax = plt.subplots()
-    data , x_e, y_e = np.histogram2d( x, y, bins = bins)
-    z = interpn( ( 0.5*(x_e[1:] + x_e[:-1]) , 0.5*(y_e[1:]+y_e[:-1]) ) , data , np.vstack([x,y]).T , method = "splinef2d", bounds_error = False )
-
-    # Sort the points by density, so that the densest points are plotted last
-    if sort :
-        idx = z.argsort()
-        x, y, z = x[idx], y[idx], z[idx]
-
-    ax.scatter( x, y, c=z, **kwargs )
-    return ax
-
-#%%
-
-db = sqlite3.connect("output.sqlite")
-sql = db.cursor() 
-
-#fingerprint = '77B829E5628712BD3D639242B9A0B40DDCB6B871'
+from density_plot import density_scatter
 
 lower = "0.0"
 higher = "10.0"
 
+### Fetch results 
+db = sqlite3.connect("output.sqlite")
+sql = db.cursor() 
 sql.execute("""
                 SELECT timestamp,latency
                 FROM output
@@ -60,9 +38,7 @@ for (t,l) in tqdm(sql.fetchall()):
     xD.append(mdates.date2num(dt)) 
     yD.append(float(l))
 
-#%%
-import matplotlib
-#matplotlib.use('Cairo')
+### General style options
 plt.rcParams['font.family'] = 'serif'
 plt.rcParams['font.serif'] = 'Ubuntu'
 plt.rcParams['font.monospace'] = 'Ubuntu Mono'
@@ -77,30 +53,38 @@ plt.rcParams['xtick.labelsize'] = 24
 plt.rcParams['ytick.labelsize'] = 24
 plt.rcParams['legend.fontsize'] = 24
 plt.rcParams['figure.titlesize'] = 24
-#plt.figure(figsize=(24,24))
 
-
-years = mdates.YearLocator()   # every year
-months = mdates.MonthLocator()  # every month
-years_fmt = mdates.DateFormatter('%Y')
-month_fmt = mdates.DateFormatter('%B')
+### Setup graph
 fig, ax = plt.subplots(1,1,figsize=(24,24),sharex=True)
 ax.set_title('All successful measurements between ' + lower +  ' and ' +higher+ ' seconds')
 ax.set_xlabel("Date (Years, Months)")
 ax.set_ylabel("Latency, excluding circuit construction (seconds)")
+
+### Setup axes 
+years = mdates.YearLocator()   
+months = mdates.MonthLocator()  
+
+years_fmt = mdates.DateFormatter('%Y')
+month_fmt = mdates.DateFormatter('%B')
+
 ax.xaxis.set_major_locator(years)
 ax.xaxis.set_major_formatter(years_fmt,)
-#ax.xaxis.set_minor_formatter(month_fmt)
 ax.xaxis.set_minor_locator(months)
+#ax.xaxis.set_minor_formatter(month_fmt)
 
 ax.set_xlim([min(xD),max(xD)])
 ax.set_ylim([float(lower),float(higher)])
 
+if len(xD) > 10000000:
+    print("WARNING: Graph may take 20+ minutes to draw and save")
+elif len(xD) > 1000000:
+    print("WARNING: Graph will take up to 20 minutes to draw and save")
+
+### Draw Graph
 x = np.array(xD)
 y = np.array(yD)
-
 density_scatter(x,y,bins=1024,ax=ax,s=200,cmap=plt.get_cmap('magma',1024))
-#plt.colorbar()
-#plt.tight_layout()
+plt.tight_layout()
+
+### Output
 plt.savefig('foo.png')
-#plt.show()
