c165d9e265ad5a47e22c8f59aaf68e61990cef63
Dennis Jackson <djackson@mozilla.com>
2019-07-15 11:02:24 -0700

    Attempt to fix the percentile estimation for bootstrapping

--- scripts/99thPercentileAny.py
+++ scripts/99thPercentileAny.py
@@ -22,6 +22,19 @@ latencyUB = '30.0'
 db = sqlite3.connect(dbpath)
 sql = db.cursor()
 
+def percentile(values, axis=1):
+    import scipy.sparse as _sparse
+    '''Returns the sum of each row of a matrix'''
+    #if isinstance(values, _sparse.csr_matrix):
+    #    ret = values.percentile(99.0,axis=axis)
+    #    return ret.A1   
+    #else:
+    if isinstance(values, _sparse.csr_matrix):
+        ret = values.percentile(99.0,axis=axis)
+        return ret.A1
+    else:
+        return np.percentile(np.asmatrix(values),99.0, axis=axis).A1
+
 def drawMedianGraph(source):
     query = """SELECT datarequest,dataresponse 
                     FROM torperf 
@@ -51,23 +64,45 @@ def drawMedianGraph(source):
     uppers = list() 
     dates = sorted(dayArrays.keys())
 
+
+
     for k in tqdm(dates,desc='Bootstrapping each day'):
         vals = np.array(dayArrays[k])
-        #r = bs.bootstrap(vals, stat_func=percentile,num_threads=12)
+        r = bs.bootstrap(vals, stat_func=percentile,num_threads=12)
         #r = bs.bootstrap(vals, stat_func=bs_stats.median,num_threads=12)
         xvalues.append(k)
-        yvalues.append(np.percentile(vals,99))
+        yvalues.append(r.value)
+        lowers.append(r.lower_bound)
+        uppers.append(r.upper_bound) 
+
+    upper_bound = go.Scatter(
+        name='Upper Bound',
+        x=xvalues,
+        y=lowers,
+        mode='lines',
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty')
 
     trace = go.Scatter(
-        name=source,
+        name='Measurement',
         x=xvalues,
         y=yvalues,
-        mode='lines')
-        #line=dict(color='rgb(31, 119, 180)'),
-        #fillcolor='rgba(255, 0, 0, 0.3)',
-        #fill='tonexty')
+        mode='lines',
+        line=dict(color='rgb(31, 119, 180)'),
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty')
 
-    return [trace]
+    lower_bound = go.Scatter(
+        name='Lower Bound',
+        x=xvalues,
+        y=uppers,
+        marker=dict(color="#444"),
+        line=dict(width=0),
+        mode='lines')
+
+    return [lower_bound,trace,upper_bound]
 
 
 layout = go.Layout(
@@ -78,4 +113,5 @@ data = list()
 
 data.extend(drawMedianGraph("All"))
 fig = go.Figure(data=data, layout=layout)
-pio.write_image(fig, '../images/torperf_median_monthly_source_ci.png',scale=4)
+plot(fig,'data.html')
+#pio.write_image(fig, '../images/torperf_median_monthly_source_ci.png',scale=)
